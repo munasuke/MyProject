7ae9cb3fe52a0af40a6c1a381bf7984578075cd4
munasuke <34709203+munasuke@users.noreply.github.com>
2018-05-12 13:50:49 +0900

    更新
    スライドパズルの移動処理終了

--- Prototype/Assets/Script/CheckObject.cs
+++ Prototype/Assets/Script/CheckObject.cs
@@ -3,22 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckObject : MonoBehaviour {
-    public string plTag = "Player";
+    public string panelTag = "Panel";
 	
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
             Ray ray = new Ray();
             RaycastHit hit = new RaycastHit();
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+            //Playerにhitしたか判定
             if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity)) {
-                if (hit.collider.gameObject.CompareTag(plTag)) {
-                    hit.collider.gameObject.GetComponent<move_sphere>().OnUserAction();
-                    Debug.Log(plTag+"よ");
-                }
-                else {
-                    Debug.Log(plTag+"じゃないよ");
+                if (hit.collider.gameObject.CompareTag(panelTag)) {
+                    hit.collider.gameObject.GetComponent<move_panel>().OnUserAction();
                 }
             }
         }
---
+++ Prototype/Assets/Script/DrawText.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawText : MonoBehaviour {
+    void OnGUI() {
+        GUI.Label(new Rect(210, 215, 150, 100), "StartPanel");
+        GUI.Label(new Rect(850, 300, 150, 100), "GoalPanel");
+    }
+}
