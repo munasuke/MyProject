6bf8a62b02ef1ab92e0fe731cbbcc593337799aa
munaoka <ra04haluhalu02@gmail.com>
2018-05-22 16:33:55 +0900

    add
    add prot

---
+++ Prototype/Assets/Script/CheckObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckObject : MonoBehaviour {
+    string[] panelTag = {
+        "Panel",//ノーマル
+        "Panel_fixed",//固定
+        "Panel_union",//合体
+        "Panel_rota"//回転
+    };
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+            Ray ray = new Ray();
+            RaycastHit hit = new RaycastHit();
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //Panelにhitしたか判定
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity)) {
+                for (int i = 0; i < panelTag.Length; i++) {
+                    if (hit.collider.gameObject.CompareTag(panelTag[i])) {
+                        hit.collider.gameObject.GetComponent<move_panel>().OnUserAction(panelTag[i]);
+                    }
+                }
+            }
+        }
+	}
+}
--- Prototype/Assets/Script/move_panel.cs
+++ Prototype/Assets/Script/move_panel.cs
@@ -3,38 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class move_panel : MonoBehaviour {
-    public float speed = 3.0f;
-    Vector3 target;
-    bool flg = false;
 
-    private void Start() {
-        target = transform.position;
-    }
-
-    private void Update () {
-        string[] panelTag = {
-            "Panel",//ノーマル
-            "Panel_fixed",//固定
-            "Panel_union",//合体
-            "Panel_rota"//回転
+    public void OnUserAction (string tag) {
+        Vector3[] vec = {
+            Vector3.back,//Down
+            Vector3.forward,//Up
+            Vector3.right,//Left
+            Vector3.left//Right
         };
+        Vector3 rota = new Vector3(0.0f, 90.0f, 0.0f);
 
-        if (Input.GetMouseButtonDown(0)) {
-            flg = true;
-        }
-        Ray ray = new Ray();
-        RaycastHit hit = new RaycastHit();
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        //Panelにhitしたか判定
-        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity)) {
-            for (int i = 0; i < panelTag.Length; i++) {
-                if (hit.collider.gameObject.CompareTag(panelTag[i])) {
-                    while (flg == true) {
-                        PanelAction(panelTag[i]);
-                        flg = false;
-                    }
+        for (int i = 0; i < 4; i++) {
+            if (CheckMove(vec[i], tag) == true) {
+                if (tag == "Panel" || tag == "Panel_union") {
+                    transform.position += vec[i]*10;
                 }
+                if (tag == "Panel_rota") {
+                    transform.Rotate(rota);
+                }
+                Debug.Log(tag);
+                break;
             }
         }
     }
@@ -70,34 +58,5 @@ public class move_panel : MonoBehaviour {
             }
         }
         return true;
-    }
-
-    //パネルの行動
-    void PanelAction(string tag) {
-        Vector3[] vec = {
-            Vector3.back,//Down
-            Vector3.forward,//Up
-            Vector3.right,//Left
-            Vector3.left//Right
-        };
-        Vector3 rota = new Vector3(0.0f, 90.0f, 0.0f);
-
-        for (int i = 0; i < vec.Length; i++) {
-            if (CheckMove(vec[i], tag) == true) {
-                if (tag == "Panel" || tag == "Panel_union") {
-                    if (target == transform.position) {
-                        target = transform.position + vec[i] * 10;
-                    }
-                    transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-                    break;
-                }
-                if (tag == "Panel_rota") {
-                    transform.Rotate(rota);
-                    break;
-                }
-                Debug.Log(tag);
-                break;
-            }
-        }
     }
 }
