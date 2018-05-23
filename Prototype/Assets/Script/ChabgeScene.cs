7ba501df1ef040b779e959582ada014b68211a2c
munaoka <ra04haluhalu02@gmail.com>
2018-05-23 18:16:28 +0900

    update

---
+++ Prototype/Assets/Script/ChabgeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChabgeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(1)) {
+            if (SceneManager.GetActiveScene().name == "main") {
+                SceneManager.LoadScene("End");
+            }
+            else {
+                SceneManager.LoadScene("main");
+            }
+        }
+	}
+}
